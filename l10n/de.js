d9222b895be0e11b56b602d53569235cdc8e71f1
Patrizio Bekerle <patrizio@bekerle.com>
2020-07-24 13:14:50 +0200

    Add German translation (#7)

--- l10n/de.js
+++ l10n/de.js
@@ -1,7 +1,11 @@
 OC.L10N.register(
     "webapppassword",
     {
-    "This page should be closed shortly!" : "Diese Seite sollte sich in Kürze schließen!",
-    "This origin is not allowed!" : "Diese Webseite ist nicht erlaubt!"
+    "Allowed origins" : "Erlaubte Webseiten",
+    "Origins that are allowed to access the page (separated by comma)" : "'Origins' mit Zugriffserlaubnis auf die Webseite (durch Beistriche getrennt)",
+    "Set origins" : "Origin-Webseiten speichern",
+    "Saved" : "Gespeichert",
+    "This origin is not allowed!" : "Diese Webseite ist nicht erlaubt!",
+    "This page should be closed shortly!" : "Diese Seite sollte sich in Kürze schließen!"
 },
 "nplurals=2; plural=n!=1;");

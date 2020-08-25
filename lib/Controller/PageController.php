c5f2c44cf6f707e17b96ad59196f388e8f60cdc9
Patrizio Bekerle <patrizio@bekerle.com>
2020-08-25 10:41:53 +0200

    Using OCP\IUserSession to fix expiring app password with OIDC (#11)

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -2,6 +2,8 @@
 
 namespace OCA\WebAppPassword\Controller;
 
+use OC\Authentication\Exceptions\InvalidTokenException;
+use OC\Authentication\Exceptions\PasswordlessTokenException;
 use OCP\AppFramework\Http;
 use OCP\IRequest;
 use OCP\AppFramework\Http\TemplateResponse;
@@ -10,15 +12,17 @@ use OCP\AppFramework\Controller;
 use OC\Authentication\Token\IProvider;
 use OC\Authentication\Token\IToken;
 use OCP\AppFramework\OCS\OCSForbiddenException;
-use OCP\Authentication\Exceptions\CredentialsUnavailableException;
-use OCP\Authentication\Exceptions\PasswordUnavailableException;
-use OCP\Authentication\LoginCredentials\IStore;
 use OCP\ISession;
+use OCP\IUserSession;
 use OCP\Security\ISecureRandom;
 use OCA\WebAppPassword\Config\Config;
+use OCP\Session\Exceptions\SessionNotAvailableException;
 
 class PageController extends Controller
 {
+    /** @var IUserSession */
+    private $userSession;
+
     /** @var ISession */
     private $session;
 
@@ -28,9 +32,6 @@ class PageController extends Controller
     /** @var IProvider */
     private $tokenProvider;
 
-    /** @var IStore */
-    private $credentialStore;
-
     /**
      * @var string[]
      */
@@ -38,19 +39,19 @@ class PageController extends Controller
 
     public function __construct(
         $AppName,
+        IUserSession $userSession,
         IRequest $request,
         ISession $session,
         ISecureRandom $random,
         IProvider $tokenProvider,
-        IStore $credentialStore,
         Config $config
     ) {
         parent::__construct($AppName, $request);
 
+        $this->userSession = $userSession;
         $this->session = $session;
         $this->random = $random;
         $this->tokenProvider = $tokenProvider;
-        $this->credentialStore = $credentialStore;
         $this->origins = $config->getOriginList();
     }
 
@@ -111,27 +112,44 @@ class PageController extends Controller
      */
     public function createToken()
     {
+        // If we are using the $this->credentialStore (OCP\Authentication\LoginCredentials\IStore)
+        // we will get empty passwords from our OIDC accounts, this causes \OC\User\Session::checkTokenCredentials
+        // to invalidate the token in the first check after 5min, using $this->session solves the problem
+        // https://gitlab.tugraz.at/dbp/nextcloud/webapppassword/-/issues/11
+
+//        try {
+//            $credentials = $this->credentialStore->getLoginCredentials();
+//        } catch (CredentialsUnavailableException $e) {
+//            throw new OCSForbiddenException();
+//        }
+//
+//        try {
+//            $password = $credentials->getPassword();
+//        } catch (PasswordUnavailableException $e) {
+//            $password = null;
+//        }
+
         try {
-            $credentials = $this->credentialStore->getLoginCredentials();
-        } catch (CredentialsUnavailableException $e) {
+            $sessionId = $this->session->getId();
+        } catch (SessionNotAvailableException $ex) {
             throw new OCSForbiddenException();
         }
 
         try {
-            $password = $credentials->getPassword();
-        } catch (PasswordUnavailableException $e) {
-            $password = null;
+            $sessionToken = $this->tokenProvider->getToken($sessionId);
+            $loginName = $sessionToken->getLoginName();
+            try {
+                $password = $this->tokenProvider->getPassword($sessionToken, $sessionId);
+            } catch (PasswordlessTokenException $ex) {
+                // this will happen for OIDC accounts
+                $password = null;
+            }
+        } catch (InvalidTokenException $ex) {
+            throw new OCSForbiddenException();
         }
 
-        // We get empty passwords from our OIDC accounts, this causes \OC\User\Session::checkTokenCredentials
-        // to invalidate the token in the first check after 5min, setting the password to null for
-        // \OC\Authentication\Token\IProvider::generateToken solves the problem
-        // https://gitlab.tugraz.at/dbp/nextcloud/webapppassword/-/issues/11
-        if ($password === "") {
-            $password = null;
-        }
-
-        $loginName = $credentials->getLoginName();
+        $uid = $this->userSession->getUser()->getUID();
+//        \OC::$server->getLogger()->warning('uid: ' . var_export($uid, true));
         $targetOrigin = $this->request->getHeader("target-origin");
         $name = $targetOrigin . ' ' . $this->request->getHeader('USER_AGENT');
         $token = $this->random->generate(
@@ -141,7 +159,7 @@ class PageController extends Controller
 
         $this->tokenProvider->generateToken(
             $token,
-            $credentials->getUID(),
+            $uid,
             $loginName,
             $password,
             $name,

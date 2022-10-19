560ea046b28f28317b015cb60fe22fae5dd948d8
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2022-10-19 11:16:59 +0200

    Add composer

--- .gitlab-ci/README.md
+++ .gitlab-ci/README.md
@@ -6,7 +6,7 @@ In case of an incompatible change increase the tag version number in build.sh!
 ./build.sh
 
 # first time only
-sudo docker login registry.gitlab.tugraz.at
+docker login registry.gitlab.tugraz.at
 
-sudo docker push registry.gitlab.tugraz.at/dbp/nextcloud/webapppassword/main:v1
+docker push registry.gitlab.tugraz.at/dbp/nextcloud/webapppassword/main:v1
 ```
--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -2,8 +2,8 @@
 
 set -e
 TAG="registry.gitlab.tugraz.at/dbp/nextcloud/webapppassword/main:v1"
-sudo docker build --tag "${TAG}" --file "Dockerfile" .
-sudo docker run --rm --security-opt label=disable \
+docker build --tag "${TAG}" --file "Dockerfile" .
+docker run --rm --security-opt label=disable \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
-echo "Now run: sudo docker push '$TAG'"
+echo "Now run: docker push '$TAG'"

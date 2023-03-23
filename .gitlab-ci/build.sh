2f9e8c84e888d5c0c7adaab77bd68417ec9bf074
Patrizio Bekerle <patrizio@bekerle.com>
2023-03-23 08:42:01 +0100

    cleanup: remove GitLab config

--- .gitlab-ci/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-TAG="registry.gitlab.tugraz.at/dbp/nextcloud/webapppassword/main:v1"
-docker build --tag "${TAG}" --file "Dockerfile" .
-docker run --rm --security-opt label=disable \
-    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
-    --tty --interactive "${TAG}" bash
-echo "Now run: docker push '$TAG'"

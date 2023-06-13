eb370dcf0e4fe152189f086a8ebbc3c596672aa9
Patrizio Bekerle <patrizio@bekerle.com>
2023-06-13 08:36:41 +0200

    nix: add dev environment

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [
+      pkgs.php82
+      pkgs.php82Packages.composer
+    ];
+}
+

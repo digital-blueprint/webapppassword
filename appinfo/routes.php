8cb2017e43c10f4148b9fed46a7d0e9f8bfa02d1
Patrizio Bekerle <patrizio@bekerle.com>
2023-08-07 09:34:48 +0200

    linter: add missing "," and fix issues

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -7,54 +7,53 @@ return [
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'page#create_token', 'url' => '/create', 'verb' => 'POST'],
         ['name' => 'admin#update', 'url' => '/admin', 'verb' => 'PUT'],
-		/*
-		 * OCS Share API
-		 */
-		[
-			'name' => 'ShareAPI#getShares',
-			'url' => '/api/v1/shares',
-			'verb' => 'GET',
-		],
-		[
-			'name' => 'ShareAPI#getInheritedShares',
-			'url' => '/api/v1/shares/inherited',
-			'verb' => 'GET',
-		],
-		[
-			'name' => 'ShareAPI#createShare',
-			'url' => '/api/v1/shares',
-			'verb' => 'POST',
-		],
-		[
-			'name' => 'ShareAPI#preflighted_cors',
-			'url' => '/api/v1/shares',
-			'verb' => 'OPTIONS',
-		],
-		[
-			'name' => 'ShareAPI#pendingShares',
-			'url' => '/api/v1/shares/pending',
-			'verb' => 'GET',
-		],
-		[
-			'name' => 'ShareAPI#getShare',
-			'url' => '/api/v1/shares/{id}',
-			'verb' => 'GET',
-		],
-		[
-			'name' => 'ShareAPI#updateShare',
-			'url' => '/api/v1/shares/{id}',
-			'verb' => 'PUT',
-		],
-		[
-			'name' => 'ShareAPI#deleteShare',
-			'url' => '/api/v1/shares/{id}',
-			'verb' => 'DELETE',
-		]
-    /*
-     * Core preview API
-     */
-    ['name' => 'Preview#getPreviewByFileId', 'url' => '/core/preview', 'verb' => 'GET'],
-    ['name' => 'Preview#preflighted_cors', 'url' => '/core/preview', 'verb' => 'OPTIONS'],
-        
+        /*
+         * OCS Share API
+         */
+        [
+            'name' => 'ShareAPI#getShares',
+            'url' => '/api/v1/shares',
+            'verb' => 'GET',
+        ],
+        [
+            'name' => 'ShareAPI#getInheritedShares',
+            'url' => '/api/v1/shares/inherited',
+            'verb' => 'GET',
+        ],
+        [
+            'name' => 'ShareAPI#createShare',
+            'url' => '/api/v1/shares',
+            'verb' => 'POST',
+        ],
+        [
+            'name' => 'ShareAPI#preflighted_cors',
+            'url' => '/api/v1/shares',
+            'verb' => 'OPTIONS',
+        ],
+        [
+            'name' => 'ShareAPI#pendingShares',
+            'url' => '/api/v1/shares/pending',
+            'verb' => 'GET',
+        ],
+        [
+            'name' => 'ShareAPI#getShare',
+            'url' => '/api/v1/shares/{id}',
+            'verb' => 'GET',
+        ],
+        [
+            'name' => 'ShareAPI#updateShare',
+            'url' => '/api/v1/shares/{id}',
+            'verb' => 'PUT',
+        ],
+        [
+            'name' => 'ShareAPI#deleteShare',
+            'url' => '/api/v1/shares/{id}',
+            'verb' => 'DELETE',
+        ],
+        /*
+         * Core preview API
+         */
+        ['name' => 'Preview#getPreviewByFileId', 'url' => '/core/preview', 'verb' => 'GET'],
+        ['name' => 'Preview#preflighted_cors', 'url' => '/core/preview', 'verb' => 'OPTIONS'],
     ],
 ];
